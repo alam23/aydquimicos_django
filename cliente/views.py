f13893f5d0899de2def27222b099bd8be5b4ca80
Anderson Alberto <andersonandyalberto23@gmail.com>
2022-01-22 05:01:23 -0500

    finalizado restriccion usuarios

--- cliente/views.py
+++ cliente/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
 
+from rest_framework import status, authentication, permissions
 from .serializers import ClienteSerializer
 from .models import Cliente
 from rest_framework.response import Response
 
 class ClienteDetalles(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self,request):
         cliente = Cliente.objects.get(user=request.user)
